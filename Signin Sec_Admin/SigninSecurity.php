17cb312c9a63af5e91c1091c27747b7fc0d3c97f
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 11:01:11 +0800

    update here

---
+++ Signin Sec_Admin/SigninSecurity.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require_once('Database.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare('SELECT * FROM users WHERE username = ? AND password = ?');
+    $stmt->bind_param('ss', $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
+        // Assuming you retrieve full name from the database query
+        $_SESSION['name'] = $user['name'];
+
+
+        // Redirect based on the user's role
+        if ($user['role'] === 'admin') {
+            header('Location: admin_system.php'); //Todo Link here the Location of admin
+        } else {
+            header('Location: security_system.php'); //Todo Link here the Location of Security
+        }
+        exit;
+    } else {
+        echo "Invalid username or password";
+    }
+
+    $stmt->close();
+}
+
+?>
