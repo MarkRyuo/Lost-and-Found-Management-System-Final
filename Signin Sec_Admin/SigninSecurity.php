2fe7d3dcca7d54a9bed7c0d124802f6e224cdaf5
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-20 00:19:21 +0800

    update here

--- Signin Sec_Admin/SigninSecurity.php
+++ Signin Sec_Admin/SigninSecurity.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($user['role'] === 'admin') {
             header('Location: ../User-Profile/userProfile.php');
         } else {
-            header('Location: /UserProfile Security/userProfileSecurity.php');
+            header('Location: /UserProfile Security/userProfile.html');
         }
         exit;
     } else {
