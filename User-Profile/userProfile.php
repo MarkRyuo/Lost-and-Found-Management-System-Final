6f2768dd9459b5fa91bf7f08ac17201a4a62b216
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 11:24:19 +0800

    update here

--- User-Profile/userProfile.php
+++ User-Profile/userProfile.php
@@ -23,6 +23,7 @@ $fullName = $_SESSION['full_name'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/User-Profile/Userprofile.css">
     <title>Admin System</title>
 </head>
 <body>
