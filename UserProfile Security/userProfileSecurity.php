fc834451b411d8763585ed71e75f519d4aff0e0a
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-20 00:24:54 +0800

    update here

--- UserProfile Security/userProfileSecurity.php
+++ UserProfile Security/userProfileSecurity.php
@@ -1,32 +1,3 @@
-<?php
-session_start();
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_nt3102";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-if (!isset($_SESSION['username'])) {
-  header('Location: userProfile.php');
-  exit;
-}
-
-if ($_SESSION['role'] !== 'admin') {
-  // Redirect if the user is not an admin
-  header('Location: /UserProfile Security/userProfileSecurity.php');
-  exit;
-}
-
-// Retrieve user information from the session
-$username = $_SESSION['username'];
-$fullName = $_SESSION['full_name'];
-?>
 
 <!DOCTYPE html>
 <html lang="en">
