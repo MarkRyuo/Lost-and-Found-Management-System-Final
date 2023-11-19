1de27f9f972823d3d8974a5b72803baf7ea2ae51
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 23:11:26 +0800

    update here

---
+++ Assets/Home-Page/Homepage-btn.js
@@ -0,0 +1,26 @@
+/*Need ng Alert dito*/
+
+
+// studentSignin btn 
+
+var studentSignin = document.getElementById("studentSignin");
+
+studentSignin.addEventListener("click", function() {
+  window.location.href = "/Student Signin/StudentSignin.php";
+});
+
+// securitySignin btn
+var securitySignin = document.getElementById("securitySignin");
+
+securitySignin.addEventListener("click", function() {
+  window.location.href = "/Security Signin/Security.php";
+});
+
+// adminSignin btn 
+var adminSignin = document.getElementById("adminSignin");
+
+adminSignin.addEventListener("click", function() {
+  window.location.href = "#";
+});
+
+
