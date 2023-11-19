ec8877320d71ca29cdec09e68f3a32eeb8123453
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 15:29:05 +0800

    update here

--- Signin Sec_Admin/SigninSecurity.php
+++ Signin Sec_Admin/SigninSecurity.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="">
+      <link rel="stylesheet" href="/Signin Sec_Admin/SigninSecurity.css">
       <title>Signin Seurity | Lost and Found</title>
   </head>
   <body>
