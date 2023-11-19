c2689b1c6cad5d912fecee8cf8fdd1e3616b20fc
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 11:25:17 +0800

    update here

--- User-Profile/userProfile.php
+++ User-Profile/userProfile.php
@@ -24,7 +24,7 @@ $fullName = $_SESSION['full_name'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/User-Profile/Userprofile.css">
-    <title>Admin System</title>
+    <title>User Profile | Lost and Found</title>
 </head>
 <body>
 
