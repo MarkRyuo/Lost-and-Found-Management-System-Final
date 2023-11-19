e2eba3e3100e142bb140e130f79761dca2047b0a
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 18:42:56 +0800

    update here

---
+++ Student/Popup.js
@@ -0,0 +1,6 @@
+// This is Close Pop up 
+
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
---
+++ Student/Student.md
@@ -0,0 +1,1 @@
+# Here the Student Sign In
