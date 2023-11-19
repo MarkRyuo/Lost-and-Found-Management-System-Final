4f782a14fb8baa0c8f7271001e49d60b9894892a
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 11:59:17 +0800

    update here

--- Assets/js/landing.js
+++
@@ -1,6 +0,0 @@
- // Lanading page here button direct to homepage
- var homepageButton = document.getElementById("homepageButton");
-
- homepageButton.addEventListener("click", function() {
-   window.location.href = "Homepage.html";
- });
