1b3272b7dd08a696f5481e999ea94ac837685130
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-21 09:07:56 +0800

    update text title

--- One/view_lost.php
+++ One/view_lost.php
@@ -8,7 +8,7 @@ include 'db.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lost Items</title>
+    <title>L Items</title>
     <style>
         .table-container {
             height: 50vh;
