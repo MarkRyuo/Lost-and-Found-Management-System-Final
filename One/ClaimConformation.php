357d502a5ecf8e46c2742a6169917b7578aa629e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-21 22:07:04 +0800

    update here

--- One/ClaimConformation.php
+++ One/ClaimConformation.php
@@ -22,6 +22,7 @@ include 'db.php';
       <link rel="stylesheet" href="/assets/css/btn-LogoutView.css">
               <!-- btn Claim Connection -->
       <link rel="stylesheet" href="/ClaimConformation/claim-btn-conformation.css">
+      <link rel="stylesheet" href="/stylecursor.css">
       <title>Claim Conformation | Lost and Found</title>
       <style>
 
