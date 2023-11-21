3f3c395e413089b08d9b733246f447ff7321e321
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-21 22:03:19 +0800

    update here

--- One/view_claimed.php
+++ One/view_claimed.php
@@ -9,6 +9,7 @@ include 'db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="img/x-icon" href="/Assets/Images/Batstatelogo.png">
+    <link rel="stylesheet" href="/stylecursor.css">
     <title>Claimed Items</title>
     <style>
         .table-container {
