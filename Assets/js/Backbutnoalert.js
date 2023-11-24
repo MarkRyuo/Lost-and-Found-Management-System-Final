ca733aaf8348888f126a05ed819760a77dcb8a4f
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-24 13:48:54 +0800

    update here

---
+++ Assets/js/Backbutnoalert.js
@@ -0,0 +1,13 @@
+
+
+var Back = document.getElementById("Back");
+
+Back.addEventListener("click", function() {
+  // Display alert
+  var confirmLogout = confirm("Are you sure you want to Back?");
+  
+  // If the user confirms, redirect to "/index.html"
+  if (confirmLogout) {
+    window.location.href = "/User-Profile/userProfile.php";
+  }
+});
